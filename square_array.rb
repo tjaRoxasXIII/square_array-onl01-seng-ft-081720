9fd1e42946fdc1a79dcad7764da743eeb5fcb625
Taylor Allen <tjaroguetech@gmail.com>
2020-08-18 13:32:01 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   i = 0
   array.each do |array_squared|
-    array_squared[i] = array_squared[i] * array_squared[i] 
+    array_squared[i] = array_squared[i] ** 2
     i += 1 
   end
 end
