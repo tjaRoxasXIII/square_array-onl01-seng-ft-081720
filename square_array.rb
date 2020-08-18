c59967b30e34584ebb5c2fc81963feb37d81bd55
Taylor Allen <tjaroguetech@gmail.com>
2020-08-18 13:50:31 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  i = 0 
+  array.each do |array_squared|
+    new_array[i] = array_squared ** 2
+    i += 1
+  end
+  return new_array
 end
